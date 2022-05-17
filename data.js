53afbb0b3dcf6ce894b6d7d8daf6a611bab80357
rubacodes <rubacodes@gmail.com>
2022-05-17 17:22:30 +0200

    inseriti ulteriori commenti

--- data.js
+++ data.js
@@ -12,10 +12,11 @@ const randomHexColorGenerator = function () {
 const printBoxes = function (array) {
   array.forEach((e) => {
     const boxHtml = tplBoxHtml.cloneNode(true);
-    //costruire il nome della classe
+    //costruire il nome della classe e assegnarlo
     const className = `fa-solid ${e.prefix}${e.name}`;
     boxHtml.querySelector('.box i').className = className;
     //2.Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
+    //bonus 1, colore hex random
     e.color = randomHexColorGenerator();
     boxHtml.querySelector('.box i').style.color = e.color;
     boxHtml.querySelector('.box span').textContent = e.name; //appendi nodo
@@ -153,7 +154,7 @@ for (let i = 0; i < allTypes.length; i++) {
      ${allTypes[i].toUpperCase()} 
     </option>`;
 }
-
+//hook ai tag necessari per la clonazione
 const container = document.querySelector('#container');
 const tplBoxHtml = document.querySelector('.tpl-box').content;
 //primo print di default al caricamento della pagina su categoria all
