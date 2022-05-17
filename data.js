18417f935d3be89324585b83d5de24948530860f
rubacodes <rubacodes@gmail.com>
2022-05-17 16:07:08 +0200

    implementazoine finale del filtraggio

--- data.js
+++ data.js
@@ -113,19 +113,31 @@ const faIcons = [
   },
 ];
 
-// Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell'icona e l'icona stessa.
-//hook al container
-const container = document.querySelector('#container');
-//
-// clono il template
-const tplBoxHtml = document.querySelector('.tpl-box').content;
-
-faIcons.forEach((e) => {
-  const boxHtml = tplBoxHtml.cloneNode(true);
-  //costruire il nome della classe
-  const className = `fa-solid ${e.prefix}${e.name}`;
-  boxHtml.querySelector('.box i').className = className;
-  boxHtml.querySelector('.box i').style.color = e.color;
-  boxHtml.querySelector('.box span').textContent = e.name; //appendi nodo
-  container.append(boxHtml);
+//3.Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
+const filterSelect = document.querySelector('#type-filter');
+let filteredArray = [];
+filterSelect.addEventListener('change', function () {
+  if (this.value === 'default') {
+    filteredArray = faIcons;
+  } else {
+    filteredArray = faIcons.filter((e) => e.type === this.value);
+  }
+  // 1.Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell'icona e l'icona stessa.
+  //hook al container
+  const container = document.querySelector('#container');
+  //refresh container al change
+  container.innerHTML = '';
+  // clono il template
+  const tplBoxHtml = document.querySelector('.tpl-box').content;
+  //popolo il container
+  filteredArray.forEach((e) => {
+    const boxHtml = tplBoxHtml.cloneNode(true);
+    //costruire il nome della classe
+    const className = `fa-solid ${e.prefix}${e.name}`;
+    boxHtml.querySelector('.box i').className = className;
+    //2.Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
+    boxHtml.querySelector('.box i').style.color = e.color;
+    boxHtml.querySelector('.box span').textContent = e.name; //appendi nodo
+    container.append(boxHtml);
+  });
 });
